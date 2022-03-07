4a663a1f4073a4758ee74984a9d5c597747369da
oscartse68 <oscartse68@gmail.com>
2022-03-07 12:55:30 +0800

    Adding PHP form

---
+++ assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,18 @@
+<?php
+class PHP_Email_Form {
+  private bool $ajax;
+  private string $to;
+  private string $from_name;
+  private string $from_email;
+  private string $subject;
+
+  // Methods
+  function add_message(...) {
+  // write the code
+  }
+
+ function send() {
+  // write the code
+  }
+}
+?>
