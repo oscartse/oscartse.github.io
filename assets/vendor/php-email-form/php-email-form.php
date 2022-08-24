936b253d3acd1cb9abb0b6d8f367d651eabe1433
oscartse68 <oscartse68@gmail.com>
2022-08-24 20:07:29 +0800

    email php

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -1,7 +1,7 @@
 <?php
 class PHP_Email_Form {
   private bool $ajax;
-  private string $to;
+  private string $toEmail;
   private string $from_name;
   private string $from_email;
   private string $subject;
@@ -13,6 +13,12 @@ class PHP_Email_Form {
 
  function send() {
   // write the code
+    $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
+    if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
+        print "<p class='success'>Mail Sent.</p>";
+    } else {
+        print "<p class='Error'>Problem in Sending Mail.</p>";
+    }
   }
 }
 ?>
